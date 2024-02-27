4e4806b0ca4e1823a76f6c83d3527e293a723800
Harivan-T <harivantahir@gmail.com>
2024-02-28 00:38:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] +", for the wonderful surprise gift!");
+    }
+    return messages;
+  }
+  
+  const names = [" Guadalupe", "Ollie", "Aki"];
+  const thankYouMessages = writeCards(names);
+  console.log(thankYouMessages);
+  
+
+  function countDown(num=4){
+    for(let i=num; i>=0;i--){
+        console.log(i);
+    };
+}
